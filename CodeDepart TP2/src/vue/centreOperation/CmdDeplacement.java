b08f31d6fec9817c5c953422ff20666673a8c272
osacontrolla <159924548+osacontrolla@users.noreply.github.com>
2024-04-04 16:29:31 -0400

    Update classe CmdDeplacement: pos cible et pos courante

--- CodeDepart TP2/src/vue/centreOperation/CmdDeplacement.java
+++ CodeDepart TP2/src/vue/centreOperation/CmdDeplacement.java
@@ -1,14 +1,48 @@
 package vue.centreOperation;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CmdDeplacement extends JPanel {
     // Classe interne pour la paire de JLabels indiquant la position courante
-    private class PositionCourantePanel extends JPanel {
+    public static class PositionCourantePanel extends JPanel {
+        public JLabel labelX; // Jlabel pour x
+        public JLabel labelY; // Jlabel pour y
 
+        public PositionCourantePanel() {
+            labelX = new JLabel("Position courante X:");
+            labelY = new JLabel("Position courante Y:");
+
+            add(labelX);
+            add(labelY);
+        }
     }
     // Classe interne publique pour le groupement JLabel + JTextField pour définir la position du Rover
     public static class PositionDefiniePanel extends JPanel {
-        
+        public JLabel labelX; // Make label public
+        public JTextField textFieldX;
+        public JLabel labelY; // Make label public
+        public JTextField textFieldY;
+
+        public PositionDefiniePanel() {
+             labelX = new JLabel("Position cible X:");
+             textFieldX = new JTextField(5); // Jtext field pour la position cible x
+             labelY = new JLabel("Position cible Y:");
+             textFieldY = new JTextField(5); //Jtext field pour la position cible y
+
+            add(labelX);
+            add(textFieldX);
+            add(labelY);
+            add(textFieldY);
+        }
+    }
+    public CmdDeplacement() {
+        setLayout(new GridLayout(2, 1)); // 2 lignes, 1 colonne
+
+        PositionCourantePanel positionCourantePanel = new PositionCourantePanel();
+        PositionDefiniePanel positionDefiniePanel = new PositionDefiniePanel();
+
+        add(positionCourantePanel);
+        add(positionDefiniePanel);
     }
 }
