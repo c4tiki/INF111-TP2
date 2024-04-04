14d94f96e76d82434407b5dcf0c1b849533c3ef6
osacontrolla <159924548+osacontrolla@users.noreply.github.com>
2024-04-04 14:50:55 -0400

    update CmdDeplacement
    ajout des 2 classes

--- CodeDepart TP2/src/vue/centreOperation/CmdDeplacement.java
+++ CodeDepart TP2/src/vue/centreOperation/CmdDeplacement.java
@@ -3,4 +3,12 @@ package vue.centreOperation;
 import javax.swing.*;
 
 public class CmdDeplacement extends JPanel {
+    // Classe interne pour la paire de JLabels indiquant la position courante
+    private class PositionCourantePanel extends JPanel {
+
+    }
+    // Classe interne publique pour le groupement JLabel + JTextField pour définir la position du Rover
+    public static class PositionDefiniePanel extends JPanel {
+        
+    }
 }
