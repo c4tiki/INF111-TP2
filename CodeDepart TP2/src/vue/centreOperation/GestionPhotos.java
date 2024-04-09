54b8c9f3beb14126f338cd00ee3a7e962b6e0784
ImaneETS <benseghir_imane@hotmail.com>
2024-04-09 16:47:13 -0400

    fin GestionPhotos et comments

--- CodeDepart TP2/src/vue/centreOperation/GestionPhotos.java
+++ CodeDepart TP2/src/vue/centreOperation/GestionPhotos.java
@@ -5,8 +5,35 @@ import java.awt.*;
 
 public class GestionPhotos extends JPanel {
 
+    private JList<String> listePhotos;
+    private JButton btnPhoto;
+    private JProgressBar barreProgres;
+
     public GestionPhotos (){
-        this.setBackground(Color.pink);
+
+        setBackground(Color.DARK_GRAY); //couleur de fond gris fonce
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); //disposition box layout verticale
+        setBorder(BorderFactory.createLineBorder(Color.WHITE)); //mettre une bordure blanche
+
+        //bouton "Prendre une photo"
+        btnPhoto = new JButton("Prendre photo");
+        btnPhoto.setAlignmentX(Component.CENTER_ALIGNMENT); //center le bouton horizontalement
+        btnPhoto.setMaximumSize(new Dimension(220, 50)); // taille du bouton max
+        add(Box.createVerticalStrut(5)); //espace entre le haut du panneau et le bouton photo
+        add(btnPhoto); //ajout de lobjet btnPhoto au panel GestionPhoto
+
+        //instanciation de la barre de progres
+        barreProgres = new JProgressBar();
+        barreProgres.setMaximumSize(new Dimension(220, 50)); //taille max barre de progres
+        add(Box.createVerticalStrut(5)); //espace entre le haut du panneau et le bouton photo
+        add(barreProgres);//ajout de lobjet barreProgres au panel GestionPhoto
+
+        //instanciation de la listePhotos
+        listePhotos = new JList<>();
+        listePhotos.setBackground(Color.WHITE); //fond blanc pour la liste
+        listePhotos.setMaximumSize(new Dimension(300, 250)); //taille max de la liste
+        add(Box.createVerticalStrut(5)); //espace entre le haut du panneau et le bouton photo
+        add(listePhotos); //ajout de lobjet listePHotos au panel GestionPhoto
 
     }
 }
