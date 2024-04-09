0300ed72467945d6830515426157a1d5d3960e18
ImaneETS <benseghir_imane@hotmail.com>
2024-04-09 17:43:21 -0400

    commentaires observeur et observable

--- CodeDepart TP2/src/observer/Observable.java
+++ CodeDepart TP2/src/observer/Observable.java
@@ -3,20 +3,23 @@ import java.util.ArrayList;
 
 
 public class Observable {
+    //creation de la liste d'observateurs
     private ArrayList<Observeur> observers = new ArrayList<>();
 
-
+    //methode permettant d'ajouter un observateur
     public void ajouter(Observeur observer) {
         observers.add(observer);
     }
-
+    //methode permettand de supprimer un observateur
     public void enlever(Observeur observer) {
         observers.remove(observer);
     }
 
+    //methode appelée quand observable change d'etat.
     public void avertirObserveur() {
+       //boucle permettant de passer a travers tous les observateurs
         for (Observeur observer : observers) {
-            observer.avertir();
+            observer.avertir(); //appel de la methode avertir de chaque observateur
         }
     }
 
--- CodeDepart TP2/src/observer/Observeur.java
+++ CodeDepart TP2/src/observer/Observeur.java
@@ -1,4 +1,5 @@
 package observer;
+//interface qui definit la methode avertir qui doit etre implementée par chaque observateur
 
 public interface Observeur {
     void avertir();
